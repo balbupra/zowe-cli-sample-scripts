636114eeaba7d29e01d98dd01db57fa11bab069a
Balbudhe Prashant <prashant.balbudhe@tieto.com>
2019-10-16 14:48:13 +0530

    r
    Signed-off-by: Balbudhe Prashant <prashant.balbudhe@tieto.com>

--- setup_credentials.sh
+++ setup_credentials.sh
@@ -1,4 +1,4 @@
 # Unlock the keyring
 echo 'jenkins' #| gnome-keyring-daemon --unlock
 # Create the Profile (move this to the credential stuff)
-zowe profiles create zosmf zed1 --host 192.49.207.105 --port 443 -u TT6PBE1 -p Prashu123 --no-ru
+#zowe profiles create zosmf zed1 --host 192.49.207.105 --port 443 -u TT6PBE1 -p Prashu123 --no-ru
