51eb0d0deebde55c6c0d954a5bff2c54e1c20799
Prashant Balbudhe <prashant.balbudhe@tieto.com>
2019-12-18 11:04:49 +0530

    test2
    Signed-off-by: Prashant Balbudhe <prashant.balbudhe@tieto.com>

--- setup_credentials.sh
+++ setup_credentials.sh
@@ -1,11 +1,11 @@
 # Unlock the keyring
 echo 'jenkins' #| gnome-keyring-daemon --unlock
 #zowe: /mnt/c/Users/balbupra/AppData/Roaming/npm/zowe /mnt/c/Users/balbupra/AppData/Roaming/npm/zowe.cmd
- /mnt/c/Users/balbupra/AppData/Roaming/npm/zowe profiles create zosmf-profile prashuzowe --host 192.49.207.105 --port 443 --tt6pbe1 --password Prashu99
+ #/mnt/c/Users/balbupra/AppData/Roaming/npm/zowe profiles create zosmf-profile prashuzowe --host 192.49.207.105 --port 443 --tt6pbe1 --password Prashu99
 
 
 # Create the Profile (move this to the credential stuff)
-#bright profiles create zosmf tso1 --host tso1 --port 443 -u $CREDENTIALS_USR -p $CREDENTIALS_PSW --no-ru
+bright profiles create zosmf tso1 --192.49.207.105 --port 443 -u tt6pbe1 -p Prashu99 --no-ru
 #zowe profiles create zosmf  --host tso1 --port 443 -u tt6pbe1 -p Prashu99 --no-ru
 #bright profiles create endevor endtso1 --host tso1 --port 50002 --user $CREDENTIALS_USR --pass $CREDENTIALS_PSW --prot http
 #bright profiles create endevor endca31 --host usilca31 --port 47400 --user $CREDENTIALS_USR --pass $CREDENTIALS_PSW --prot http
